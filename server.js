c820613c23463152ea38dd940024caf3829055ef
PauloPortilho <pc_marttins@hotmail.com>
2021-05-25 20:18:39 -0400

    Aula 25-04

--- server.js
+++ server.js
@@ -15,6 +15,3 @@ http.createServer(function(req, res){
     //var query = url.parse(req.url, true).query;
     //res.end("Login:"+query.login + " - " + "Senha"+query.senha);
 }).listen(8080);
-
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
