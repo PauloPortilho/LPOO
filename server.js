11de357d8239ed1344872e8e25cc3a5197749db0
PauloPortilho <pc_marttins@hotmail.com>
2021-05-25 20:17:40 -0400

    Aula dia 25

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+//var dt = require('./time');
+var url = require('url');
+var fs = require('fs');
+
+
+http.createServer(function(req, res){
+    fs.readFile('index.html', function(err, data){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write(data);
+    return res.end();
+    })
+
+    
+    //var query = url.parse(req.url, true).query;
+    //res.end("Login:"+query.login + " - " + "Senha"+query.senha);
+}).listen(8080);
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
